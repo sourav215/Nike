44356c2ead7add3f6dc334d271a8c2c0f1f3e3d8
sourav215 <sourav10010080@gmail.com>
2023-02-12 20:40:38 +0530

    Modified order controller

--- server/models/order.model.js
+++ server/models/order.model.js
@@ -1,60 +1,13 @@
-const { Schema, model } = require('mongoose');
+const mongoose = require('mongoose');
+const crypto = require('crypto');
 
-const reqString = { type: String,  };
-const reqNumber = { type: Number,  };
-const reqArray = { type: Array,   };
+const reqString = { type: String, required: true };
+const reqNumber = { type: Number, required: true };
+const reqArray = { type: Array, required: true };
 
+const OrderSchema = new Schema(
 
-const OrderSchema = new Schema({
-
-    orderSummary: {
-        subTotal: reqNumber,
-        quantity: reqNumber,
-        shipping: reqNumber,
-        discount: reqNumber,
-        total: reqNumber
-    },
-    cartProducts: [
-        {
-            title: reqString,
-            gender: reqString,
-            description: reqString,
-            category: reqString,
-            price: reqNumber,
-            size: reqString,
-            color: reqString,
-            rating: reqNumber,
-            img: reqArray,
-            quantity: reqNumber
-        }
-    ],
-    paymentDetails: {
-        orderId: reqString,
-        razorpayOrderId: reqString,
-        razorpayPaymentId: reqString,
-    },
-    shippingDetails: {
-        firstName: reqString,
-        lastName: reqString,
-        addressLine1: reqString,
-        addressLine2: { type: String, required: false },
-        locality: reqString,
-        pinCode: reqNumber,
-        state: reqString,
-        country: reqString,
-        email: reqString,
-        mobile: reqNumber
-    },
-    userId: {
-        type: Schema.Types.ObjectId,
-        ref: 'user',
-      
-    }
-
-}, {
-    versionKey: false,
-    timestamps: true
-});
+    );
 
 const Order = model("Order", OrderSchema);
 
