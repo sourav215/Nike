cd19ef8372205910ad9641103377e20ecf90d0c6
sourav215 <sourav10010080@gmail.com>
2023-02-13 16:05:59 +0530

    Modified controller

--- server/index.js
+++ server/index.js
@@ -1,17 +1,16 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const connectDatabase = require('./config/db');
-const authController = require('./controllers/authController');
-const clothDataController = require('./controllers/clothDataController')
+const connectDatabase = require("./config/db");
+const authController = require("./controllers/authController");
+const clothDataController = require("./controllers/clothDataController");
 const ShoeController = require("./controllers/shoeController");
 const productController = require("./controllers/productController");
 const cartController = require("./controllers/cartController");
-const paymentController = require("./controllers/payment.controller")
-const orderController = require("./controllers/order.controller");
 
+const orderController = require("./controllers/order.controller");
 
 const app = express();
 
@@ -19,30 +18,23 @@ app.use(express.json());
 
 app.use(cors());
 
-
 app.get("/", (req, res) => {
-    res.send("Welcome to Nike ")
-})
-app.use('/users', authController)
-const PORT = 8080;
-app.use("/clothData",clothDataController)
-app.use("/shoeData",ShoeController)
- 
+  res.send("Welcome to Nike ");
+});
+
+app.use("/users", authController);
+app.use("/clothData", clothDataController);
+app.use("/shoeData", ShoeController);
 app.use("/products", productController);
- 
- 
-app.use("/cart",cartController);
+app.use("/cart", cartController);
 app.use("/orders", orderController);
-app.use("/payment", paymentController);
 
-
-
-
+const PORT = 8080;
 app.listen(PORT, () => {
-    try {
-        connectDatabase();
-        console.log('Server is listening...')
-    } catch (err) {
-        console.log(err);
-    }
-})
+  try {
+    connectDatabase();
+    console.log("Server is listening...");
+  } catch (err) {
+    console.log(err);
+  }
+});
