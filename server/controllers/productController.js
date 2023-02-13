bc613c44f798acc5db818489285ac52016659e01
sourav215 <sourav10010080@gmail.com>
2023-02-13 12:45:50 +0530

    Modified controller

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,49 +1,45 @@
-const express = require("express")
-const ProductModel = require("../models/ProductModel")
-const authentication = require('../middlewares/authentication')
+const express = require("express");
+const ProductModel = require("../models/ProductModel");
+const authentication = require("../middlewares/authentication");
 
-const productController = express.Router()
+const productController = express.Router();
 
+productController.get("/", async (req, res) => {
+  const products = await ProductModel.find();
 
-productController.get("/", async(req,res)=>{
+  res.send(products);
+});
+productController.get("/men", async (req, res) => {
+  const products = await ProductModel.find({ gender: "Men" });
 
- const products = await ProductModel.find()
- 
- res.send(products)
-})
-productController.get("/men", async(req,res)=>{
+  res.send(products);
+});
+productController.get("/women", async (req, res) => {
+  // console.log("req.url",req.url)
+  const products = await ProductModel.find({ gender: "Women" });
+  // console.log(products)
+  res.send(products);
+});
+productController.get("/kids", async (req, res) => {
+  const products = await ProductModel.find({ gender: "Kids" });
 
- const products = await ProductModel.find({gender:"Men"})
- 
- res.send(products)
-})
-productController.get("/women", async(req,res)=>{
-// console.log("req.url",req.url)
- const products = await ProductModel.find({gender:"Women"})
- // console.log(products)
- res.send(products)
-})
-productController.get("/kids", async(req,res)=>{
+  res.send(products);
+});
+productController.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
 
- const products = await ProductModel.find({gender:"Kids"})
- 
- res.send(products)
-})
-productController.get("/:id", async(req,res)=>{
+    const product = await ProductModel.findOne({ _id: id });
+    // console.log("product",product)
+    res.send(product);
+  } catch (err) {
+    res.send("Empty");
+  }
+});
 
- const {id} = req.params
- 
- const product = await ProductModel.findOne({_id:id})
- // console.log("product",product)
- res.send(product)
-})
-productController.post("/",(req,res)=>{
+productController.post("/", (req, res) => {
+  const products = ProductModel.insertMany(req.body);
+  res.send(products);
+});
 
- const products = ProductModel.insertMany(req.body)
- res.send(products)
-})
-
-
- 
-
-module.exports = productController 
+module.exports = productController;
