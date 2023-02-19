ad4e274f8bc0e3a1a7a96ca46167a74d7591135c
sourav215 <sourav10010080@gmail.com>
2023-02-19 19:43:31 +0530

    Modified home components

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import Recommend from "../../components/homecomponents/Recommend.jsx";
+import Slideshow from "../../components/homecomponents/Slideshow.jsx";
+
+export const Home = () => {
+  return (
+    <>
+      <Slideshow />
+
+      <Recommend />
+    </>
+  );
+};
