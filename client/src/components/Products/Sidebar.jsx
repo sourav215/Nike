2a25f6b3de834822c9b2b6e7b267813737ebf2d5
sourav215 <sourav10010080@gmail.com>
2023-02-20 22:17:46 +0530

    Modified Product

---
+++ client/src/components/Products/Sidebar.jsx
@@ -0,0 +1,12 @@
+import { Box, Grid, VStack } from '@chakra-ui/react'
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <Grid templateColumns={["1fr", "2fr 2fr", "1fr 3fr"]} justifyContent="flex-end">
+
+    </Grid>
+  )
+}
+
+export default Sidebar
