f08bc96d44b64753bc440e87be83a93029e528c6
sourav215 <sourav10010080@gmail.com>
2023-02-22 21:25:48 +0530

    Modified Cart

---
+++ client/src/components/cart/CartShow.jsx
@@ -0,0 +1,110 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  Image,
+  Stack,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
+import React from "react";
+import { useDispatch } from "react-redux";
+import {
+  countCartData,
+  deleteCartData,
+  getCartData,
+} from "../../redux/AppReducer/action";
+import { setToast } from "../../utilties/toastfun";
+
+const CartShow = ({ title, img, price, _id, count }) => {
+  const dispatch = useDispatch();
+  const handleChange = (id, type) => {
+    if (count === 1 && type === "dec") {
+      // console.log("count is", count);
+      dispatch(deleteCartData(id)).then((res) => dispatch(getCartData()));
+    } else {
+      dispatch(countCartData(id, type)).then((res) => dispatch(getCartData()));
+    }
+  };
+  const toast = useToast();
+  const handleRemoveItem = (id) => {
+    setToast(toast, "Removed Succesfully", "info");
+    dispatch(deleteCartData(id)).then((res) => dispatch(getCartData()));
+  };
+
+  const handleAddToFavourite = () => {
+    setToast(toast, "Availble Soon", "warning");
+  };
+
+  return (
+    <>
+      <Flex justifyContent={"space-between"}>
+        <Box display={"flex"} gap={["5px", "5px", "20px", "20px", "20px"]}>
+          <Box
+            w={["80px", "80px", "150px", "150px", "150px"]}
+            h={["80px", "80px", "150px", "150px", "150px"]}
+          >
+            <Image h={"85%"} src={img[0]} />
+          </Box>
+
+          <Box
+            w={"100%"}
+            display={"grid"}
+            gap={"2%"}
+            gridTemplateColumns={[
+              "67% 30%",
+              "67% 30%",
+              "80% 18%",
+              "80% 18%",
+              "80% 18%",
+            ]}
+          >
+            <Box minH={"150px"}>
+              <Text fontWeight={500}>{title}</Text>
+              {/* <Text color={'gray'}>{description}</Text> */}
+
+              <Flex alignItems={"center"} gap={"10px"} my={"8px"}>
+                <Text>Quantity:</Text>
+
+                <Button onClick={() => handleChange(_id, "dec")}>-</Button>
+                <Text fontWeight={600}>{count}</Text>
+
+                <Button onClick={() => handleChange(_id, "inc")}>+</Button>
+              </Flex>
+
+              <Box display={"flex"} gap={"10px"} textDecoration={"underline"}>
+                <Text
+                  cursor={"pointer"}
+                  title={"Favourites"}
+                  onClick={handleAddToFavourite}
+                >
+                  Favourite
+                </Text>
+
+                <Text
+                  cursor={"pointer"}
+                  title={"Remove"}
+                  onClick={() => handleRemoveItem(_id)}
+                >
+                  Remove
+                </Text>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+        <Box minH={"150px"}>
+          <Text fontSize={"20px"} fontWeight="600" marginTop="5px">
+            ₹{price}
+          </Text>
+        </Box>
+      </Flex>
+
+      <Divider />
+    </>
+  );
+};
+
+export default CartShow;
