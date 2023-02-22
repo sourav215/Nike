33b458eaaea10168c561f2c46c6594c3eb1c8e6f
sourav215 <sourav10010080@gmail.com>
2023-02-22 21:25:26 +0530

    Modified Cart

---
+++ client/src/components/cart/BtnCheckout.jsx
@@ -0,0 +1,19 @@
+import { Button } from "@chakra-ui/react";
+
+export const BtnCheckout = ({ onClick }) => {
+  return (
+    <>
+      <Button
+        onClick={onClick}
+        h={"60px"}
+        border={`1px solid black`}
+        borderRadius={"50px"}
+        w={"100%"}
+        fontSize={"17px"}
+        mb={"20px"}
+      >
+        Checkout Delivery
+      </Button>
+    </>
+  );
+};
