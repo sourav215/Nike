aadfe2708f659abc51fbbc4d4b18c33d6f305c64
sourav215 <sourav10010080@gmail.com>
2023-02-22 21:27:05 +0530

    Modified

---
+++ client/src/components/gender/MenShow.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+import { Grid, GridItem } from "@chakra-ui/react";
+
+import AllProduct from "../Products/AllProduct";
+import { Loading } from "../loading/Loading";
+
+const MenShow = ({ mens }) => {
+  const loading = useSelector((state) => state.AppReducer.notLoading);
+  if (loading === false) {
+    return <Loading />;
+  }
+  return (
+    <Grid
+      gap={["10px", "10px", "20px", "20px", "20px"]}
+      templateColumns={[
+        "repeat(1, 1fr)",
+        "repeat(2, 1fr)",
+        "repeat(2, 1fr)",
+        "repeat(3, 1fr)",
+        "repeat(4, 1fr)",
+      ]}
+      justifyContent="flex-end"
+    >
+      {mens?.map((el, i) => (
+        <GridItem>
+          <AllProduct key={i} {...el} />
+        </GridItem>
+      ))}
+    </Grid>
+  );
+};
+
+export default MenShow;
